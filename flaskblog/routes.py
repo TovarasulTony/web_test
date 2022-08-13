ad29a923ded01cc2b0134a91c734e32c4fd27478
Chunchunmarx <andreeaoprescu12@yahoo.com>
2022-08-14 01:46:34 +0300

    project

---
+++ flaskblog/routes.py
@@ -0,0 +1,18 @@
+import sys
+import pathlib
+
+# I really don't limke this hack
+#sys.path.insert(0, str(pathlib.Path(__file__).parent.parent.parent))
+
+import os
+import secrets
+from PIL import Image
+from flask import render_template, url_for, flash, redirect, request, abort, send_from_directory, jsonify
+from flaskblog import app, db, bcrypt, mail
+
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('index.html'
---
+++ run.py
@@ -0,0 +1,5 @@
+from flaskblog import app
+
+
+if __name__ == "__main__":
+    app.run(host="194.135.95.157", port=1077)
